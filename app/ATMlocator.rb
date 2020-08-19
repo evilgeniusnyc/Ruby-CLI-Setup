027e2d503b01df9655ef49002155f5d97da0a1ba
wilsonng21 <wilsongch92@gmail.com>
2020-08-18 23:02:08 -0400

    can go back to main menu now

--- app/ATMlocator.rb
+++ app/ATMlocator.rb
@@ -6,10 +6,13 @@ class ATMlocator
     @prompt = TTY::Prompt.new
   end
 
+
   def friendly_bank_list
     prompt = TTY::Prompt.new 
-    prompt.ok("You are a customer at these banks --#{self.user.user_bank_list}--")
+    prompt.ok("You are a customer at these banks --#{self.user_bank_list}--" )
+    puts "Bank list takes a day to update =/"
   end
+
 
   def login_or_register
     user_choice = self.prompt.select("Logging in or Registering?",
@@ -30,28 +33,33 @@ class ATMlocator
     end
   end
 
+
   def main_menu
+    system "clear"
     puts "-----------------------------------------"
     puts "Hello #{self.user.user_name}, this is the main menu."
 
-    if self.user.user_bank_list.length != 0
+    if self.user_bank_list.length != 0
       self.friendly_bank_list
+      puts "Bank list takes a day to update =/"
     else
       Messages.no_bank_message
+      puts "*Bank list takes a day to update to ensure accuracy =/*"
     end
     puts "-----------------------------------------"
 
     self.prompt.select("What would you like to accomplish today?") do |menu|
       menu.choice "Find banks by zipcode", -> { self.list_banks_by_zipcode }
-      menu.choice "Add your bank to a list of banks to which you are a customer", -> { self.user.add_bank_to_list }
-      menu.choice "Add/Update favorite bank location(s)", -> { puts "--method here to update userbank row 'user_fav attribute'--"}
+      menu.choice "Add your bank to a list of banks to which you are a customer", -> { self.add_bank_to_list }
       menu.choice "Delete a bank from your list", -> { puts "--Userbank find associated row and .destroy here to disassociate--" }
-      menu.choice "Not what you are looking for? Click here for more.", -> { puts "--sub_menu here--"}
+      menu.choice "Not what you are looking for? Click here for more.".yellow, -> { puts "--sub_menu here--"}
       menu.choice "Log off".red, -> { puts "--Have a nice day!--"}
     end
   end
 
+
   def list_banks_by_zipcode
+    system "clear"
     prompt = TTY::Prompt.new
     zipcode_input = prompt.ask("What zipcode do you want to look into?", required: true)
 
@@ -66,8 +74,36 @@ class ATMlocator
     keypress = prompt.keypress("***Press enter to go back to main menu***".yellow, keys: [:return])
 
     self.main_menu if keypress
+  end
+
+
+  def user_bank_list
+    self.user.banks.map { |bank_instance| "#{bank_instance.bank_name}" }.uniq 
+  end #returns an array of uniq bank names associated with user
+
+
+  def add_bank_to_list
+    prompt = TTY::Prompt.new
+    bank_instance_id = prompt.ask("Please add a bank by their bank id number =] -->", required: true) { |q| q.in("1-5612") }
+
+    new_userbank = Userbank.create(user_id: self.user.id, bank_id: bank_instance_id)
+
+    sleep 1
+
+    new_bank = Bank.find(new_userbank.bank_id)
+    if !self.user_bank_list.include? new_bank.bank_name 
+        puts "Here's the updated bank list for you --> #{self.user_bank_list.uniq << new_bank.bank_name}".green
+    else
+        Userbank.last.destroy #destroys the newly created Userbank isntance instead of implementing conditional
+        puts "Duplication breh."
+    end
+
+    keypress = prompt.keypress("***Press enter to go back to main menu***".yellow, keys: [:return])
+
+    self.main_menu if keypress
 
   end
+
 
   def run
     Messages.welcome
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,41 +17,8 @@ class User < ActiveRecord::Base
         prompt =  TTY::Prompt.new
         user_name = prompt.ask("Please input your username -->", required: true)
 
-        sleep 1
-
-        puts "You are registered!"
-        puts "-----------------------------------------"
-        puts "Your user id is unique to you and it's --#{User.create_unique_id}--."
-        puts "Please commit it to your memory."
-
         User.create(user_name: user_name)
         # thinking about adding another column to users table just for fun, assigning a 6 digit "unique_id" to each user.
     end
-
-    def self.create_unique_id
-        rand.to_s[2..7]
-    end
-
-    def user_bank_list
-        self.banks.map { |bank_instance| "#{bank_instance.bank_name}" }.uniq 
-    end #returns an array of uniq bank names
-
-    def add_bank_to_list
-        prompt = TTY::Prompt.new
-        bank_instance_id = prompt.ask("Please add a bank by their bank id number =] -->", required: true) { |q| q.in("1-5612") }
-
-        new_userbank = Userbank.create(user_id: self.id, bank_id: bank_instance_id)
-
-        sleep 1
-
-        new_bank = Bank.find(new_userbank.bank_id)
-        if !self.user_bank_list.include? new_bank.bank_name 
-            puts "Here's the updated bank list for you --> #{self.user_bank_list.uniq << new_bank.bank_name}".green
-        else
-            Userbank.last.destroy #destroys the newly created Userbank isntance instead of implementing conditional
-            puts "Duplication breh."
-        end
-    end
-
     
 end
